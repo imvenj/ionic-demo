62311d2175108fbf95989f03f1356879651a25fa
Venj <ersaclarke@gmail.com>
2021-01-28 11:30:58 +0800

    ActionSheet Demo.

--- src/views/Demo.vue
+++ src/views/Demo.vue
@@ -12,7 +12,7 @@
     <ion-content class="ion-padding">
       <ion-list>
         <ion-item detail v-for="demo in demos" :key="demo.path">
-          <ion-icon :icon="scan" slot="start"></ion-icon>
+          <ion-icon :icon="bookmarkOutline" slot="start"></ion-icon>
           <ion-label @click="router.push(`/tabs/tab1/demo/${demo.path}`)">{{ demo.name }}</ion-label>
         </ion-item>
       </ion-list>
@@ -38,7 +38,7 @@ import {
 import { defineComponent } from 'vue'
 import { useRouter } from 'vue-router'
 import { demos } from '@/config/DemoNames'
-import { scan } from 'ionicons/icons'
+import { bookmarkOutline } from 'ionicons/icons'
 
 export default defineComponent({
   name: 'Demo',
@@ -58,11 +58,16 @@ export default defineComponent({
   setup() {
     const router = useRouter()
 
-    return { router, demos, scan }
+    return { router, demos, bookmarkOutline }
   }
 })
 </script>
 
-<style>
-
+<style scoped>
+ion-icon {
+  padding: 5px;
+  background-color: var(--ion-color-primary);
+  color: white;
+  border-radius: 50%;
+}
 </style>
--- src/views/demos/ActionSheet.vue
+++ src/views/demos/ActionSheet.vue
@@ -10,7 +10,11 @@
     </ion-header>
 
     <ion-content>
-
+      <div class="ion-text-center ion-padding">
+        <ion-button @click="showActionSheet">
+          Show Action Sheet
+        </ion-button>
+      </div>
     </ion-content>
   </ion-page>
 </template>
@@ -24,8 +28,10 @@ import {
   IonBackButton,
   IonContent,
   IonTitle,
-  IonToolbar
+  IonToolbar,
+  actionSheetController
 } from '@ionic/vue'
+import { shareOutline } from 'ionicons/icons'
 
 export default defineComponent({
   name: 'ActionSheet',
@@ -39,7 +45,43 @@ export default defineComponent({
     IonToolbar
   },
   setup() {
-    return {}
+    const showActionSheet = async () => {
+      const actionSheet = await actionSheetController.create({
+        header: 'Albumn',
+        buttons: [
+          {
+            text: 'Delete',
+            role: 'destructive',
+            handler: () => {
+              console.log('Delete handler')
+            }
+          },
+          {
+            text: 'Share',
+            icon: shareOutline,
+            handler: () => {
+              console.log('Share handler')
+            }
+          },
+          {
+            text: 'Favorite',
+            handler: () => {
+              console.log('Favorite handler')
+            }
+          },
+          {
+            text: 'Cancel',
+            role: 'cancel',
+            handler: () => {
+              console.log('Cancel handler')
+            }
+          }
+        ]
+      })
+
+      return actionSheet.present()
+    }
+    return { showActionSheet }
   }
 })
 </script>
