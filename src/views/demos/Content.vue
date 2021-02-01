1eaeabc4359241242bc08c34aba5295cb1be89d9
Venj <ersaclarke@gmail.com>
2021-02-01 13:34:43 +0800

    Content.

--- src/views/demos/Content.vue
+++ src/views/demos/Content.vue
@@ -9,14 +9,33 @@
       </ion-toolbar>
     </ion-header>
 
-    <ion-content>
-
+    <ion-content
+      ref="content"
+      :scroll-events="true"
+      @ionScrollStart="logScrollStart"
+      @ionScroll="logScrolling($event)"
+      @ionScrollEnd="logScrollEnd">
+      <div class="ion-padding">
+        <ion-button expand="block" outline @click="scrollToBottom">
+          Scroll To Bottom
+        </ion-button>
+      </div>
+      <ion-list>
+        <ion-item v-for="i in 30" :key="`key-${i}`">
+          <ion-label>Item {{i}}</ion-label>
+        </ion-item>
+      </ion-list>
+      <div class="ion-padding">
+        <ion-button expand="block" outline @click="scrollToTop">
+          Scroll To Top
+        </ion-button>
+      </div>
     </ion-content>
   </ion-page>
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue'
+import { ref, defineComponent } from 'vue'
 import {
   IonPage,
   IonHeader,
@@ -24,6 +43,10 @@ import {
   IonBackButton,
   IonContent,
   IonTitle,
+  IonButton,
+  IonList,
+  IonItem,
+  IonLabel,
   IonToolbar
 } from '@ionic/vue'
 
@@ -36,10 +59,38 @@ export default defineComponent({
     IonBackButton,
     IonContent,
     IonTitle,
+    IonButton,
+    IonList,
+    IonItem,
+    IonLabel,
     IonToolbar
   },
   setup() {
-    return {}
+    // TODO: Notice how to use ref in Vue 3!
+    const content = ref()
+
+    const logScrollStart = () => {
+      console.log('Scroll started!')
+    }
+
+    const logScrolling = ($event: CustomEvent) => {
+      // TODO: Dig into scroll event object.
+      console.log('scrolling: ', $event.detail.deltaY)
+    }
+
+    const logScrollEnd = () => {
+      console.log('Scroll ended!')
+    }
+
+    const scrollToBottom = () => {
+      content.value.$el.scrollToBottom(300);
+    }
+
+    const scrollToTop = () => {
+      content.value.$el.scrollToTop(300);
+    }
+
+    return { content, logScrollStart, logScrolling, logScrollEnd, scrollToBottom, scrollToTop }
   }
 })
 </script>
