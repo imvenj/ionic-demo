78e93ddea166e1363ef276d4dea347dde7e05092
Venj <ersaclarke@gmail.com>
2021-02-01 11:15:32 +0800

    Chip.

--- src/views/demos/Chip.vue
+++ src/views/demos/Chip.vue
@@ -10,6 +10,108 @@
     </ion-header>
 
     <ion-content>
+      <ion-list-header>
+        Basic
+      </ion-list-header>
+      <div class="ion-padding">
+        <ion-chip>
+          <ion-label>Default</ion-label>
+        </ion-chip>
+        <ion-chip>
+          <ion-icon :icon="checkmarkCircle"></ion-icon>
+          <ion-label>Icon</ion-label>
+        </ion-chip>
+        <ion-chip>
+          <ion-avatar>
+            <img src="/assets/shapes.svg" alt="">
+          </ion-avatar>
+          <ion-label>Avatar</ion-label>
+        </ion-chip>
+      </div>
+
+      <ion-list-header>
+        Colors
+      </ion-list-header>
+      <div class="ion-padding">
+        <ion-chip color="primary">
+          <ion-label>Primary</ion-label>
+        </ion-chip>
+        <ion-chip color="secondary">
+          <ion-label>Secondary</ion-label>
+        </ion-chip>
+        <ion-chip color="tertiary">
+          <ion-label>Tertiary</ion-label>
+        </ion-chip>
+        <ion-chip color="success">
+          <ion-label>Success</ion-label>
+        </ion-chip>
+        <ion-chip color="warning">
+          <ion-label>Warning</ion-label>
+        </ion-chip>
+        <ion-chip color="danger">
+          <ion-label>Danger</ion-label>
+        </ion-chip>
+        <ion-chip color="light">
+          <ion-label>Light</ion-label>
+        </ion-chip>
+        <ion-chip color="medium">
+          <ion-label>Medium</ion-label>
+        </ion-chip>
+        <ion-chip color="dark">
+          <ion-label>dark</ion-label>
+        </ion-chip>
+      </div>
+
+      <ion-list-header>
+        Outline
+      </ion-list-header>
+
+      <div class="ion-padding">
+        <ion-chip outline color="primary">
+          <ion-label>Primary</ion-label>
+        </ion-chip>
+        <ion-chip outline color="secondary">
+          <ion-label>Secondary</ion-label>
+        </ion-chip>
+        <ion-chip outline color="tertiary">
+          <ion-label>Tertiary</ion-label>
+        </ion-chip>
+        <ion-chip outline color="success">
+          <ion-label>Success</ion-label>
+        </ion-chip>
+        <ion-chip outline color="warning">
+          <ion-label>Warning</ion-label>
+        </ion-chip>
+        <ion-chip outline color="danger">
+          <ion-label>Danger</ion-label>
+        </ion-chip>
+        <ion-chip outline color="light">
+          <ion-label>Light</ion-label>
+        </ion-chip>
+        <ion-chip outline color="medium">
+          <ion-label>Medium</ion-label>
+        </ion-chip>
+        <ion-chip outline color="dark">
+          <ion-label>dark</ion-label>
+        </ion-chip>
+      </div>
+
+      <ion-list-header>
+        Icon
+      </ion-list-header>
+
+      <div class="ion-padding">
+        <ion-chip outline color="primary">
+          <ion-icon :icon="pin"></ion-icon>
+          <ion-label>Madison</ion-label>
+          <ion-icon :icon="closeCircle"></ion-icon>
+        </ion-chip>
+        <ion-chip outline color="secondary">
+          <ion-icon :icon="heart"></ion-icon>
+          <ion-label>Madison</ion-label>
+          <ion-icon :icon="closeCircle"></ion-icon>
+        </ion-chip>
+      </div>
 
     </ion-content>
   </ion-page>
@@ -24,8 +126,15 @@ import {
   IonBackButton,
   IonContent,
   IonTitle,
+  IonChip,
+  IonLabel,
+  IonListHeader,
+  IonIcon,
+  IonAvatar,
   IonToolbar
 } from '@ionic/vue'
+
+import { close, closeCircle, checkmarkCircle, heart, pin } from 'ionicons/icons'
 
 export default defineComponent({
   name: ' Chip',
@@ -36,10 +145,15 @@ export default defineComponent({
     IonBackButton,
     IonContent,
     IonTitle,
+    IonChip,
+    IonLabel,
+    IonListHeader,
+    IonIcon,
+    IonAvatar,
     IonToolbar
   },
   setup() {
-    return {}
+    return { close, closeCircle, checkmarkCircle, heart, pin }
   }
 })
 </script>
