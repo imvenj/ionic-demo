9f6dd65683f0c8638e91ae5dcf66ebdf57730a76
imvenj <im.venj@gmail.com>
2021-03-10 16:20:52 +0800

    Menu.

--- src/views/demos/Menu.vue
+++ src/views/demos/Menu.vue
@@ -5,13 +5,34 @@
         <ion-buttons slot="start">
           <ion-back-button default-href="/"></ion-back-button>
         </ion-buttons>
+        <ion-buttons slot="end">
+          <ion-menu-button menu="first"></ion-menu-button>
+        </ion-buttons>
         <ion-title>Menu</ion-title>
       </ion-toolbar>
     </ion-header>
 
-    <ion-content>
-
+    <ion-menu side="start" menu-id="first" content-id="main">
+      <ion-header>
+        <ion-toolbar>
+          <ion-title>My Menu</ion-title>
+        </ion-toolbar>
+      </ion-header>
+      <ion-content>
+        <ion-list>
+          <ion-item>Menu Item</ion-item>
+          <ion-item>Menu Item</ion-item>
+          <ion-item>Menu Item</ion-item>
+          <ion-item>Menu Item</ion-item>
+          <ion-item>Menu Item</ion-item>
+        </ion-list>
+      </ion-content>
+    </ion-menu>
+    
+    <ion-content class="ion-padding" id="main">
+      <ion-button expand="block" @click="showMenu">Show Menu</ion-button>
     </ion-content>
+
   </ion-page>
 </template>
 
@@ -24,7 +45,13 @@ import {
   IonBackButton,
   IonContent,
   IonTitle,
-  IonToolbar
+  IonToolbar,
+  IonMenu,
+  IonList,
+  IonItem,
+  menuController,
+  IonMenuButton,
+  IonButton
 } from '@ionic/vue'
 
 export default defineComponent({
@@ -36,10 +63,19 @@ export default defineComponent({
     IonBackButton,
     IonContent,
     IonTitle,
-    IonToolbar
+    IonToolbar,
+    IonMenu,
+    IonList,
+    IonItem,
+    IonMenuButton,
+    IonButton
   },
   setup() {
-    return {}
+    const showMenu = () => {
+      menuController.enable(true, 'first');
+      menuController.open('first');
+    }
+    return { showMenu }
   }
 })
 </script>
