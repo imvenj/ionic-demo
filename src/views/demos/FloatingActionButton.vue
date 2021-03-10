155539a3162cdba122a2dbe58fa927bb94de13ea
imvenj <im.venj@gmail.com>
2021-03-10 11:06:02 +0800

    FAB.

--- src/views/demos/FloatingActionButton.vue
+++ src/views/demos/FloatingActionButton.vue
@@ -10,6 +10,20 @@
     </ion-header>
 
     <ion-content>
+
+      
+      <ion-fab vertical="center" horizontal="center" slot="fixed">
+        <ion-fab-button>
+          <ion-icon :icon="add"></ion-icon>
+        </ion-fab-button>
+      </ion-fab>
+
+      <ion-fab vertical="top" horizontal="center" edge slot="fixed">
+        <ion-fab-button>
+          <ion-icon :icon="add"></ion-icon>
+        </ion-fab-button>
+      </ion-fab>
+
       <ion-fab vertical="top" horizontal="end" slot="fixed">
         <ion-fab-button>
           <ion-icon :icon="add"></ion-icon>
@@ -21,53 +35,38 @@
           <ion-icon :icon="arrowForwardCircle"></ion-icon>
         </ion-fab-button>
       </ion-fab>
+
+      <!-- fab placed to the top start -->
+      <ion-fab vertical="top" horizontal="start" slot="fixed">
+        <ion-fab-button>
+          <ion-icon :icon="arrowBackCircle"></ion-icon>
+        </ion-fab-button>
+      </ion-fab>
+
+      <!-- fab placed to the bottom and start and on the bottom edge of the content overlapping footer with a list to the right -->
+      <ion-fab vertical="bottom" horizontal="start" slot="fixed">
+        <ion-fab-button>
+          <ion-icon :icon="settings"></ion-icon>
+        </ion-fab-button>
+        <ion-fab-list side="end">
+          <ion-fab-button><ion-icon :icon="logoTwitter"></ion-icon></ion-fab-button>
+        </ion-fab-list>
+      </ion-fab>
+
+      <!-- fab placed to the (vertical) center and start -->
+      <ion-fab vertical="center" horizontal="start" slot="fixed">
+        <ion-fab-button>
+          <ion-icon :icon="share"></ion-icon>
+        </ion-fab-button>
+      </ion-fab>
+
+      <!-- fab placed to the (vertical) center and end -->
+      <ion-fab vertical="center" horizontal="end" slot="fixed">
+        <ion-fab-button>
+          <ion-icon :icon="add"></ion-icon>
+        </ion-fab-button>
+      </ion-fab>
     </ion-content>
-
-    <!-- fab placed to the top start -->
-    <ion-fab vertical="top" horizontal="start" slot="fixed">
-      <ion-fab-button>
-        <ion-icon :icon="arrowBackCircle"></ion-icon>
-      </ion-fab-button>
-    </ion-fab>
-
-    <!-- fab placed to the bottom start -->
-    <ion-fab vertical="bottom" horizontal="start" slot="fixed">
-      <ion-fab-button>
-        <ion-icon :icon="arrowUpCircle"></ion-icon>
-      </ion-fab-button>
-    </ion-fab>
-
-    <!-- fab placed to the (vertical) center and start -->
-    <ion-fab vertical="center" horizontal="start" slot="fixed">
-      <ion-fab-button>
-        <ion-icon :icon="share"></ion-icon>
-      </ion-fab-button>
-    </ion-fab>
-
-    <!-- fab placed to the (vertical) center and end -->
-    <ion-fab vertical="center" horizontal="end" slot="fixed">
-      <ion-fab-button>
-        <ion-icon :icon="add"></ion-icon>
-      </ion-fab-button>
-    </ion-fab>
-
-    <!-- fab placed to the top and end and on the top edge of the content overlapping header -->
-    <ion-fab vertical="top" horizontal="end" edge slot="fixed">
-      <ion-fab-button>
-        <ion-icon :icon="person"></ion-icon>
-      </ion-fab-button>
-    </ion-fab>
-
-    <!-- fab placed to the bottom and start and on the bottom edge of the content overlapping footer with a list to the right -->
-    <ion-fab vertical="bottom" horizontal="start" edge slot="fixed">
-      <ion-fab-button>
-        <ion-icon :icon="settings"></ion-icon>
-      </ion-fab-button>
-      <ion-fab-list side="end">
-        <ion-fab-button><ion-icon :icon="logoVimeo"></ion-icon></ion-fab-button>
-      </ion-fab-list>
-    </ion-fab>
-
   </ion-page>
 </template>
 
@@ -82,12 +81,12 @@ import {
   IonTitle,
   IonFab,
   IonFabButton,
-  // IonFabList,
+  IonFabList,
   IonIcon,
   IonToolbar
 } from '@ionic/vue'
 
-import { add, arrowForwardCircle, arrowBackCircle, arrowUpCircle, share, person, settings, logoVenmo } from 'ionicons/icons'
+import { add, arrowForwardCircle, arrowBackCircle, arrowUpCircle, share, person, settings, logoTwitter } from 'ionicons/icons'
 
 export default defineComponent({
   name: ' FloatingActionButton',
@@ -100,12 +99,12 @@ export default defineComponent({
     IonTitle,
     IonFab,
     IonFabButton,
-    // IonFabList,
+    IonFabList,
     IonIcon,
     IonToolbar
   },
   setup() {
-    return { add, arrowForwardCircle, arrowBackCircle, arrowUpCircle, share, person, settings, logoVenmo }
+    return { add, arrowForwardCircle, arrowBackCircle, arrowUpCircle, share, person, settings, logoTwitter }
   }
 })
 </script>
