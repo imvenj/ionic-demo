71058c4fd328d5c58cb866a44719176e3914a226
imvenj <im.venj@gmail.com>
2021-03-11 09:54:37 +0800

    Note

--- src/views/demos/Note.vue
+++ src/views/demos/Note.vue
@@ -10,6 +10,56 @@
     </ion-header>
 
     <ion-content>
+      <ion-list-header>Basic</ion-list-header>
+      <ion-grid>
+        <ion-row>
+          <ion-col><ion-note color="default">Default</ion-note></ion-col>
+          <ion-col><ion-note color="primary">Primary</ion-note></ion-col>
+          <ion-col><ion-note color="secondary">Secondary</ion-note></ion-col>
+          <ion-col><ion-note color="tertiary">Tertiary</ion-note></ion-col>
+        </ion-row>
+        <ion-row>
+          <ion-col><ion-note color="success">Success</ion-note></ion-col>
+          <ion-col><ion-note color="warning">Warning</ion-note></ion-col>
+          <ion-col><ion-note color="danger">Danger</ion-note></ion-col>
+          <ion-col><ion-note color="dark">Dark</ion-note></ion-col>
+        </ion-row>
+      </ion-grid>
+      <ion-list>
+        <ion-list-header>List Notes</ion-list-header>
+        <ion-item>
+          <ion-label>Default</ion-label>
+          <ion-note color="default" slot="end">11</ion-note>
+        </ion-item>
+        <ion-item>
+          <ion-label>Primary</ion-label>
+          <ion-note color="primary" slot="end">22</ion-note>
+        </ion-item>
+        <ion-item>
+          <ion-label>Secondary</ion-label>
+          <ion-note color="secondary" slot="end">33</ion-note>
+        </ion-item>
+        <ion-item>
+          <ion-label>Tertiary</ion-label>
+          <ion-note color="tertiary" slot="end">44</ion-note>
+        </ion-item>
+        <ion-item>
+          <ion-label>Success</ion-label>
+          <ion-note color="success" slot="start">55</ion-note>
+        </ion-item>
+        <ion-item>
+          <ion-label>Warning</ion-label>
+          <ion-note color="warning" slot="start">66</ion-note>
+        </ion-item>
+        <ion-item>
+          <ion-label>Danger</ion-label>
+          <ion-note color="danger" slot="start">77</ion-note>
+        </ion-item>
+        <ion-item>
+          <ion-label>Dark</ion-label>
+          <ion-note color="dark" slot="start">88</ion-note>
+        </ion-item>
+      </ion-list>
 
     </ion-content>
   </ion-page>
@@ -24,7 +74,15 @@ import {
   IonBackButton,
   IonContent,
   IonTitle,
-  IonToolbar
+  IonToolbar,
+  IonNote,
+  IonList,
+  IonItem,
+  IonLabel,
+  IonListHeader,
+  IonGrid,
+  IonCol,
+  IonRow
 } from '@ionic/vue'
 
 export default defineComponent({
@@ -36,7 +94,15 @@ export default defineComponent({
     IonBackButton,
     IonContent,
     IonTitle,
-    IonToolbar
+    IonToolbar,
+    IonNote,
+    IonList,
+    IonItem,
+    IonLabel,
+    IonListHeader,
+    IonGrid,
+    IonCol,
+    IonRow
   },
   setup() {
     return {}
