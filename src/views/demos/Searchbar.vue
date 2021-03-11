69c78237ed606197b87af0380767e47a1558c6fa
imvenj <im.venj@gmail.com>
2021-03-11 17:04:10 +0800

    Search bar.

--- src/views/demos/Searchbar.vue
+++ src/views/demos/Searchbar.vue
@@ -7,16 +7,23 @@
         </ion-buttons>
         <ion-title>Searchbar</ion-title>
       </ion-toolbar>
+      <ion-toolbar>
+        <ion-searchbar v-model="keyword" slot=""></ion-searchbar>
+      </ion-toolbar>
     </ion-header>
 
     <ion-content>
-
+      <ion-list>
+        <ion-item v-for="c in filteredCities" :key="c">
+          <ion-label>{{ c }}</ion-label>
+        </ion-item>
+      </ion-list>
     </ion-content>
   </ion-page>
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue'
+import { defineComponent, ref, computed } from 'vue'
 import {
   IonPage,
   IonHeader,
@@ -24,7 +31,11 @@ import {
   IonBackButton,
   IonContent,
   IonTitle,
-  IonToolbar
+  IonToolbar,
+  IonSearchbar,
+  IonList,
+  IonItem,
+  IonLabel
 } from '@ionic/vue'
 
 export default defineComponent({
@@ -36,10 +47,22 @@ export default defineComponent({
     IonBackButton,
     IonContent,
     IonTitle,
-    IonToolbar
+    IonToolbar,
+    IonSearchbar,
+    IonList,
+    IonItem,
+    IonLabel
   },
   setup() {
-    return {}
+    const keyword = ref('')
+    const cities = [
+      'Amsterdam', 'Bogota', 'Cairo', 'Dhaka', 'Geneva', 'Glasgow', 'Hanoi', 'Hong Kong', 'Istanbul',
+      'Jakatar', 'Kiel', 'Kyoto', 'Le havre', 'Lebanon', 'Lima', 'London', 'Los Angeles', 'Madrid',
+      'Manila', 'Oslo', 'Panama City', 'Peking', 'San Francisco', 'Seoul', 'Taipeh', 'Tokyo', 'Washington'
+    ]
+    const filteredCities = computed(() => cities.filter(c => c.toLowerCase().startsWith(keyword.value.toLowerCase())))
+
+    return { keyword, filteredCities }
   }
 })
 </script>
